eb8dd8aae188a99b46d7f1f12c317be9859b295d
Krushna Koshatwar <krushnakoshatwaar619@gmail.com>
2026-08-26 03:10:04 +0530

    3rd continued

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,3 +11,15 @@ console.log(valueInNumber);
 // "33abc"=> NaN 
 //true => 1; fales => 0
 // 1 => true; 0 => false 
+
+
+// ******************operations***************** 
+
+let value = 3 
+let negVlue = -value 
+
+
+let str1 = "hello world"
+let str2 = "krushna"
+let str3 = str1 + str2
+console.log(str3); 
