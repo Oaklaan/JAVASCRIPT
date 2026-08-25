a5f8874ca0b972c999a5c259fa48e1d4ce2b77a3
Krushna Koshatwar <krushnakoshatwaar619@gmail.com>
2026-08-26 02:27:42 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 12345
+let accountEmail = "kk@gmail.com"
+var accountPassword = "13245678"
+accountCity = "jaipur"
+let accountState;
+
+// accountID = 2 //not allowed
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountCity, accountState]);
+
+
+ /*
+preffer not to use var because of issue in block scope and  functional scope
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version 
+
+console.log(4
+    + 4
+) // code redeability is important
+
+let name = "krushna"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 3  
+// bigint
+// string
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "hitesh")
