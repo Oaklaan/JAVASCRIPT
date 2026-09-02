972337e6c238ca5d333bd524928cf103ef72ff72
Krushna Koshatwar <krushnakoshatwaar619@gmail.com>
2026-09-02 15:13:01 +0530

    update datatypes

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -7,3 +7,17 @@ const scoreValue = 100
 // refference (non primitives)
 
 // Array, Objects, Functions  
+
+
+
+let myName = "krushna"
+let myName2 = "krushna koshatwar"
+
+console.log(myName, myName2) 
+
+let userOne = {
+    email: "krushna@gmail.com",
+    upi: "user@ybl"
+}
+
+console.log( userOne.email); 
