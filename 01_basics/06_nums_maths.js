b27192599a34f047acec9ed7cb5a5e93af4a2611
Krushna Koshatwar <krushnakoshatwar619@gmail.com>
2026-09-04 10:19:51 +0530

    update

--- 01_basics/06_nums_maths.js
+++ 01_basics/06_nums_maths.js
@@ -34,3 +34,7 @@ const max = 20
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
 // console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
