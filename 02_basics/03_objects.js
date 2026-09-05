bbdeee9e28fff8c8a6f821f72b0e92cf900026cf
Krushna Koshatwar <krushnakoshatwar619@gmail.com>
2026-09-05 19:19:39 +0530

    03 objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,19 +9,27 @@ const jsuser = {
     id: 123,
     state: "maharstra",
     isLoggedIn: false,
-    lastlogindays: ["monday", "tuesday"]
+    lastlogindays: ["monday", "tuesday"],
+    email: "krushnakoshatwar@gmail.com"
 }
 
-console.log(jsuser.name)
-console.log(jsuser["name"])
-// console.log(typeof jsuser.mysym)
-console.log(jsuser[mysym])
-
-
-
-
-
-
-
+// console.log(jsuser.name)
 // console.log(jsuser["name"])
- 
+// console.log(typeof jsuser.mysym)
+// console.log(jsuser[mysym])
+// console.log(jsuser["name"])
+
+jsuser.email = "hitesh@chatgpt.com"
+// Object.freeze(jsuser)
+jsuser.email = "hitesh@microsoft.com"
+// console.log(jsuser);
+
+jsuser.greeting = function(){
+    console.log("Hello JS user");
+}
+jsuser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(jsuser.greeting());
+
+
