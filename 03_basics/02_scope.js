9cbfd5d94c8246c10c58dc3ab2bf3221baad3cbc
Krushna Koshatwar <krushnakoshatwar619@gmail.com>
2026-09-10 14:54:34 +0530

    scope.js final push

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,6 +6,35 @@ if (true) {
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
 // something (var) inside the curly barces is local scope if u console var it will provide that value even if u not provide the var declaaration . var outside the curly braces is called the global scope. 
+
+function one(){
+
+const username = "krushna"
+    function two(){
+        const website = "kk.com"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+// in this we have called the one() function outside & console.log(username) is a local scope calling outside its own function so it will print the value .
+
+if (true){
+    const username = "krushna"
+    if (username === "krushna"){
+        const website = " youtube"
+        //console.log(username + website);
+        
+    }
+     //console.log(website);
+    
+}
+
+// console.log(username);
+
+//--------------------------------------- 
