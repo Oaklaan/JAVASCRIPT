c75ea99ba61c90b07b50bc5577066832015f6bdd
Krushna Koshatwar <krushnakoshatwar619@gmail.com>
2026-09-10 14:00:39 +0530

    functions and scope commit

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,11 @@
+if (true) {
+    let a = 23
+    const b = 50
+    var c = 40
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+// something (var) inside the curly barces is local scope if u console var it will provide that value even if u not provide the var declaaration . var outside the curly braces is called the global scope. 
