4f468c0a479c38256eb45e2fcdcadfe1bbacb22a
Krushna Koshatwar <krushnakoshatwar619@gmail.com>
2026-09-19 11:13:47 +0530

    basics completed with code and callstack

---
+++ 03_basics/05_callstack.js
@@ -0,0 +1,15 @@
+function one (){
+    console.log("1")
+}
+function two (){
+    console.log("2")
+}
+function three (){
+    console.log("3")
+}
+one()
+two()
+three() 
+
+
+// in primary browser engine implimentation is structured demonstrated. each function will sequentially one afte another (onw two and three respectively)
